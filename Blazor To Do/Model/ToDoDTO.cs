b114febfbd9bfe5e256ee53fde6ff67f68670969
Jimna <jimmynarokian254@gmail.com>
2023-09-13 16:48:46 +0300

    Add project files.

---
+++ Blazor To Do/Model/ToDoDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Blazor_To_Do.Model
+{
+    public class ToDoDTO
+    {
+        public Guid Id { get; set; }  
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Priority { get; set; }
+
+        public DateTime DateCreated { get; set; }  = DateTime.Now;
+
+
+    }
+}
